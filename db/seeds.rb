91fc741a40f979f500fb25b53006ee32e0739859
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-04 22:18:04 +0200

    Added missing comma.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 banks = Bank.create([{ name: 'Millenium Bim' }, { name: 'BCI' }, { name: 'Moza Banco' }])
 participant = Participant.create(name: 'Admilson Cossa')
-account = Account.create( number: '210309408' holder: participant, bank: banks.first)
+account = Account.create( number: '210309408', holder: participant, bank: banks.first)
 financial_categories = FinancialCategory.create([{ name: 'Production' }, { name: 'Administration' }, { name: 'Comercial' }])
