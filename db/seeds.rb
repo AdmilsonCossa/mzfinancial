3f1af2333979845c59310e9f9d8bab12c22115d9
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-04 22:05:29 +0200

    Added records needed to seed the database with its defaults values.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+banks = Bank.create([{ name: 'Millenium Bim' }, { name: 'BCI' }, { name: 'Moza Banco' }])
+participant = Participant.create(name: 'Admilson Cossa')
+account = Account.create( number: '210309408' holder: participant, bank: banks.first)
+financial_categories = FinancialCategory.create([{ name: 'Production' }, { name: 'Administration' }, { name: 'Comercial' }])
