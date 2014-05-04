e11fda1f3b3a2c5e71d07c0a4e43482d3ae4afcd
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-04 22:18:59 +0200

    Added validations to the main account fields.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,18 @@
 class Account < ActiveRecord::Base
   #attr_accessible :balance, :number
+  
   belongs_to :holder, 
     class_name: 'Participant',
     foreign_key: 'holder_id'
+  
   belongs_to :bank 
+
+  validates :number,
+    presence: true
+
+  validates :holder,
+    presence: true
+
+  validates :bank,
+    presence: true
 end
