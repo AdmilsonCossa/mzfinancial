f7f71714d5ceb37cf93da089db26bee83e914368
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-04 23:29:34 +0200

    Added method execute_transfer.

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -7,4 +7,25 @@ class Transfer < ActiveRecord::Base
   validates :origin_account, :destination_account, :value
       presence: true
   
+  def execute_transfer!
+    raise "Transference already executed!" if state == 'executed'
+      if origin_account.balance >= value
+        origin_account.debit(value)
+        destination_account.credit(value)
+        self.state = 'executed'
+        self.state_datetime = Time.zone.now
+      else
+        self.state = 'failed'
+        self.state_datetime = Time.zone.now
+        elf.state_reason = 'Not enough balance!'
+      end
+      self.save
+      self
+    rescue Exception => ex
+      self.state = 'failed'
+      self.state_datetime = Time.zone.now
+      self.state_reason = ex.message
+      self.save!
+      self
+  end
 end
