ffba252b6432e7cf5c21dd7bf2c16377151d134c
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-21 16:07:39 +0200

    Added autocomplte.

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,18 +1,27 @@
 class Account < ActiveRecord::Base
   #attr_accessible :balance, :number
   
+  autocomplete :display_name,
+   result_fetcher: ->(query) do
+      joins(:Participant).where('LOWER("participants"."name") LIKE LOWER(?)',
+        autocomplete_query(query),
+      ).order('"participants"."name"')
+    end
+
   belongs_to :holder, 
     class_name: 'Participant',
     foreign_key: 'holder_id'
   
   belongs_to :bank 
 
-  validates :number,
+  validates :number, :holder, :bank,
     presence: true
 
-  validates :holder,
-    presence: true
+  def display_name
+    "#{holder.name} #{number || '<ephemeral>'}"
+  end
 
-  validates :bank,
-    presence: true
+  def to_s
+    display_name
+  end
 end
