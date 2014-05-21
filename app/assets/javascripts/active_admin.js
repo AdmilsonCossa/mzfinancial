51937ef487cbdcb6af88ad214b80799d8f6a9f0c
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-21 16:09:52 +0200

    Added require to jquery token-input.

---
+++ app/assets/javascripts/active_admin.js
@@ -0,0 +1,10 @@
+//= require active_admin/base
+//= require autocomplete
+
+// $(document).ready(function(){
+
+// $('#main_content').on('ajax:success', '.relationship-table .pagination a', function(e, html){
+//   $(this).closest('.relationship-table').replaceWith(html);
+// });
+// });
+
