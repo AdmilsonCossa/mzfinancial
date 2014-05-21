39fd478990022fb808210d2a31f262cc40d494fc
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-21 18:48:27 +0200

    Added transfer transaction.

---
+++ app/models/transaction_orders/transfer.rb
@@ -0,0 +1,31 @@
+class Transfer < ActiveRecord::Base
+  #attr_accessible :origin_account, :destination_account, :value
+
+  belongs_to :origin_account, class_name: 'Account', foreign_key: 'id'
+  belongs_to :destination_account, class_name: 'Account', foreign_key: 'id'
+
+  validates :origin_account, :destination_account, :value,
+      presence: true
+  
+  def execute_transfer!
+    raise "Transference already executed!" if state == 'executed'
+      if origin_account.balance >= value
+        origin_account.debit(value)
+        destination_account.credit(value)
+        self.state = 'executed'
+        self.state_date = Time.zone.now
+      else
+        self.state = 'failed'
+        self.state_date = Time.zone.now
+        elf.state_reason = 'Not enough balance!'
+      end
+      self.save
+      self
+    rescue Exception => ex
+      self.state = 'failed'
+      self.state_date = Time.zone.now
+      self.state_reason = ex.message
+      self.save!
+      self
+  end
+end
