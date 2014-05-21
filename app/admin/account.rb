6253de79861c588693a8b1663f457a021c52e280
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-21 16:06:24 +0200

    renamed permited attributes.

--- app/admin/account.rb
+++ app/admin/account.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Account do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-   permit_params :number, :holder, :bank
+   permit_params :number, :holder_id, :bank_id
   # or
   #
   # permit_params do
