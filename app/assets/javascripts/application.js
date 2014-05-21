ee83462484988f7cefbdd53f304963a342499c50
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-21 16:20:34 +0200

    !

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require highchart/highcharts
+//= require highchart/highstocks
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
