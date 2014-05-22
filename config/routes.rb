a8b1bb94235b8606b9e7319ac6b7108feb3b43cf
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-22 18:42:58 +0200

    Fixed routes.

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-      resources :autocomplete
+  #  resources :autocomplete
 
   # Example resource route with options:
   #   resources :products do
