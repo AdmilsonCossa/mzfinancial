fec54d04a5c63b1f7e732b1edbf1383c2607b7cf
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-27 09:51:50 +0200

    Fixed method initialize states!

--- app/models/accounts_payable.rb
+++ app/models/accounts_payable.rb
@@ -12,7 +12,24 @@ class AccountsPayable < ActiveRecord::Base
   belongs_to :expense_type
   belongs_to :account
 
- validates :participant, :financial_category, :expense_type, :account, :value,
-  :document_serie, :document_number, :issue_date, :due_date,
+  after_create :check_due_date
+  before_save  :initialize_states
+
+  validates :participant, :financial_category, :expense_type, :account, :value,
+    :document_serie, :document_number, :issue_date, :due_date,
     presence: true
+
+  def check_due_date
+    # You can now check the "due" field here. For example, if you only want to allow due
+    # dates today and later:
+    if self.due_date < Date.today or self.due_date < self.issue_date
+      errors.add(:due_date, "can only be today or later.")
+    end
+  end
+
+  def initialize_states
+    self.state = 'not factured'
+    self.term_state = 'in time' if Date.today <= self.due_date
+  end
+
 end
