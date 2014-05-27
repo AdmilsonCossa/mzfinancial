94d4c3de877534b41f5f620b04a34af340d008e3
Admilson Cossa <admilsoncossa@gmail.com>
2014-05-27 09:51:11 +0200

    Added custom index.

--- app/admin/accounts_payable.rb
+++ app/admin/accounts_payable.rb
@@ -4,7 +4,9 @@ ActiveAdmin.register AccountsPayable do
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  # permit_params :list, :of, :attributes, :on, :model
+  permit_params :participant_id, :document_serie, :document_number,
+    :issue_date, :due_date, :value, :account_id,
+    :financial_category_id, :expense_type_id
   #
   # or
   #
@@ -13,8 +15,6 @@ ActiveAdmin.register AccountsPayable do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
-  permit_params :participant, :account, :financial_category, :description, :expense_type
 
   controller do
     def scoped_collection
@@ -23,6 +23,22 @@ ActiveAdmin.register AccountsPayable do
         :account,
       )
       collection
+    end
+  end
+
+  index do
+    selectable_column
+    id_column
+    column :due_date
+    column :participant
+    column :description
+    column :state do |ac_r| status_tag(ac_r.state, (ac_r.state == 'factured' ? :ok : :warning)) end
+    column :value
+    column :account
+    column :term_state do |ac_r| status_tag(ac_r.term_state, (ac_r.term_state == 'In time' ? :ok : :red)) end
+    
+    actions default: true do |acc_rec| 
+      link_to('confirm', confirm_admin_accounts_receivable_path(acc_rec))
     end
   end
 
@@ -42,4 +58,20 @@ ActiveAdmin.register AccountsPayable do
 
     f.actions
   end
+
+  member_action :facture, :method => :put do
+    @acc_payee = AccountsReceivable.find(params[:id])
+
+    if @acc_payee.state == 'factured'
+      flash[:alert] = "Already factured"
+      redirect_to :action => :index
+    else
+      acc_payee.confirm_payee!
+      redirect_to(
+        confirm_admin_accounts_payable_path(resource.facture),
+        notice: "Payable sucessfully factured."
+      )
+    end
+  end
 end
+    
